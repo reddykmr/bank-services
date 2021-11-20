24ffdfdd1be36533fb3ec6474919b224c420c29c
karna-mahesh <mahesh.karna@thermofisher.com>
2021-11-20 13:06:11 +0530

    second

--- employee-data-managment/src/main/java/com/example/demo/EmployeeDataManagmentApplication.java
+++ employee-data-managment/src/main/java/com/example/demo/EmployeeDataManagmentApplication.java
@@ -8,6 +8,7 @@ public class EmployeeDataManagmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeDataManagmentApplication.class, args);
+		
 		System.out.println(" hello");
 	}
 
