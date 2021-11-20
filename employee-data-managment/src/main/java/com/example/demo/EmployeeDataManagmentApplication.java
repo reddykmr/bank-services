bb85ae717ae40c35a4f567efcf3db7343825ecc4
karna-mahesh <mahesh.karna@thermofisher.com>
2021-11-20 13:01:27 +0530

    sysout

---
+++ employee-data-managment/src/main/java/com/example/demo/EmployeeDataManagmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDataManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDataManagmentApplication.class, args);
+		System.out.println(" hello");
+	}
+
+}
